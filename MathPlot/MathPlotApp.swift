3d748b4db382d96da7a9a8609e2131153bcaa87c
RezaHemadi <r.hemadi@gmail.com>
2023-05-14 11:05:30 +0330

    Initial Commit

---
+++ MathPlot/MathPlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathPlotApp.swift
+//  MathPlot
+//
+//  Created by Reza on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MathPlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
